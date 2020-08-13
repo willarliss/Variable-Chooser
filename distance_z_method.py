69eb46aff99d4b9bee7a8b325002d3b5850c858e
Will <warliss98@gmail.com>
2020-08-12 21:52:47 -0400

    Update distance_z_method.py

--- distance_z_method.py
+++ distance_z_method.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
@@ -214,41 +215,3 @@ class VariableChooser:
         return self.n_combos
   
     
-    
-def test():    
-    
-    # Generate synthetic data
-    np.random.seed(3) 
-    df = pd.DataFrame(np.random.randint(1,9, 100), columns=['a'])
-    df['b'] = df['a'] + np.random.randn(100)
-    df['c'] = df['b'] + np.random.randn(100)
-    df['d'] = df['c'] + np.random.randn(100)
-    df['e'] = df['d'] + np.random.randn(100)
-    df['f'] = df['e'] + np.random.randn(100)
-    df['g'] = df['f'] + np.random.randn(100)
-    df['h'] = df['g'] + np.random.randn(100)
-    df.drop('a', axis=1, inplace=True)
-    
-    vc = VariableChooser(df, ['b','c','d','f','g','h'], 'e')
-    
-    indep, dep = vc.correlations(method='weight', minimum=2, len_penalty=True)
-    solution = vc.solve()
-    
-    n = 5
-    for i in np.arange(n):
-        combo = list(solution[i][1][0])    
-        print(solution[i]) # rank, combo, distance
-        print('-'*22)
-        print(df[combo].corr()) # correl of combination
-        print(1-vc.dep_corr[tuple(combo)]) # Agg corr to dep var
-        print()
-    
-    vc.plot(tol=0.689)
-    
-    
-  
-if __name__ == '__main__':
-    
-    test()
-
-
