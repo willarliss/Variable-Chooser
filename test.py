9883d7456e55831cb4a0dd343c9f1ec2697436ab
Will <warliss98@gmail.com>
2020-08-13 15:28:41 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,21 @@
-import os
 import sys
 import numpy as np
 import pandas as pd
-from distance_z_method import VariableChooser
 
+from z_distance_combos import VariableChooser, z_distance, total_combos
 
+def main():
+    
+    data = pd.read_csv('mtcars.csv')
+    cols = list(col for col in data.columns if col not in ['car', 'mpg'])
+            
+    vc = VariableChooser(data, 'mpg')
 
-def main():    
+    solution = vc.select_combo(cols, minimum=3, len_penalty=True)
     
-    # Generate synthetic data
-    np.random.seed(3) 
-    df = pd.DataFrame(np.random.randint(1,9, 100), columns=['a'])
-    df['b'] = df['a'] + np.random.randn(100)
-    df['c'] = df['b'] + np.random.randn(100)
-    df['d'] = df['c'] + np.random.randn(100)
-    df['e'] = df['d'] + np.random.randn(100)
-    df['f'] = df['e'] + np.random.randn(100)
-    df['g'] = df['f'] + np.random.randn(100)
-    df['h'] = df['g'] + np.random.randn(100)
-    df.drop('a', axis=1, inplace=True)
-    
-    vc = VariableChooser(df, ['b','c','d','f','g','h'], 'e')
-    
-    indep, dep = vc.correlations(method='weight', minimum=2, len_penalty=True)
-    solution = vc.solve()
-    
-    n = 5
-    for i in np.arange(n):
-        combo = list(solution[i][1][0])    
-        print(solution[i])
-        print('-'*22)
-        print(df[combo].corr())
-        print(1-vc.dep_corr[tuple(combo)])
-        print()
-    
-    vc.plot(tol=0.689)
-    
-    
-    
+    for i in np.arange(10):
+        print(vc.solutions[i])
+        
 if __name__ == '__main__':
-    
     main()
-    
-    
     
