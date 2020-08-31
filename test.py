36cbd071aaa2de90d922f574f0b4a7e03fe13e8f
Will <warliss98@gmail.com>
2020-08-30 22:35:41 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-import sys
-import numpy as np
 import pandas as pd
-
+from sklearn.datasets import load_boston
 from z_distance_combos import VariableChooser, z_distance, total_combos
 
-def main():
-    
-    data = pd.read_csv('mtcars.csv')
-    cols = list(col for col in data.columns if col not in ['car', 'mpg'])
-            
-    vc = VariableChooser(data, 'mpg')
+boston = load_boston()
+df = pd.DataFrame(boston.data, columns=[list(boston.feature_names)])
+df.columns = [col[0] for col in df.columns.values]
+df['PRICE'] = boston.target
 
-    solution = vc.select_combo(cols, minimum=3, len_penalty=True)
-    
-    for i in np.arange(10):
-        print(vc.solutions[i])
-        
-if __name__ == '__main__':
-    main()
+print(z_distance(df, ['INDUS', 'NOX', 'AGE', 'TAX', 'RAD'], 'PRICE'))
+print(z_distance(df, ['DIS', 'RM', 'PTRATIO', 'LSTAT', 'TAX'], 'PRICE'))
+
+vc = VariableChooser(df, 'PRICE')
+
+cols = df.drop('PRICE', axis=1).columns
+solution = vc.select_combo(cols, minimum=3, len_penalty=True)
+print(solution)
+
+for i in np.arange(10):
+    print(vc.solutions[i])
     
